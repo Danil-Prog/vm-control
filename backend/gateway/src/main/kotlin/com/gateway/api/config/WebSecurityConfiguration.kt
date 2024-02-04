99964b0307f96b1472000fff8c1f71af0e6f96d5
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-02-04 23:46:07 +0700

    configuration cors

--- backend/gateway/src/main/kotlin/com/gateway/api/config/WebSecurityConfiguration.kt
+++ backend/gateway/src/main/kotlin/com/gateway/api/config/WebSecurityConfiguration.kt
@@ -6,7 +6,6 @@ import com.gateway.api.config.jwt.JWTConverter
 import com.gateway.api.config.jwt.JWTTokenWebFilter
 import com.gateway.api.service.ExternalUserDetailsService
 import com.gateway.api.service.JwtService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -24,6 +23,10 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
@@ -32,9 +35,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 @EnableWebFlux
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
-class WebSecurityConfiguration(
-    @Value("\${gateway.cors.allowed-origins}") val URL: String
-) : WebFluxConfigurer {
+class WebSecurityConfiguration : WebFluxConfigurer {
 
     @Bean
     fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
@@ -45,11 +46,14 @@ class WebSecurityConfiguration(
         jwtAuthenticationFilter: AuthenticationWebFilter,
         jwtService: JwtService
     ): SecurityWebFilterChain {
+        http.cors {
+            it.configurationSource(corsConfiguration())
+        }
         return http.authorizeExchange { exchange ->
             exchange
                 .pathMatchers(*EXCLUDED_PATH).permitAll()
                 .anyExchange().authenticated()
-        }.csrf { csrf -> csrf.disable() }.cors { cors -> cors.disable() }
+        }.csrf { csrf -> csrf.disable() }
             .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             .addFilterAt(JWTTokenWebFilter(jwtService), SecurityWebFiltersOrder.AUTHORIZATION)
             .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
@@ -88,10 +92,29 @@ class WebSecurityConfiguration(
         }
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/")
-            .allowedOrigins(URL)
+        registry.addMapping("/**")
+            .allowedOrigins("*")
             .allowedMethods("*")
     }
+
+    @Bean
+    fun corsConfiguration(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowCredentials = true
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("Access-Control-Allow-Origin",
+            "Authorization", "Content-Type")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun corsWebFilter(source: CorsConfigurationSource): CorsWebFilter =
+        CorsWebFilter(source)
+
 
     companion object {
         val EXCLUDED_PATH = arrayOf("/auth/refresh", "/", "/static/**", "/favicon.ico")
