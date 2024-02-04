7cc981bf408b93337a1aa82a962589a2bf6e78f9
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-02-04 21:24:49 +0700

    update setting web client

--- backend/gateway/src/main/kotlin/com/gateway/api/config/WebClientConfiguration.kt
+++ backend/gateway/src/main/kotlin/com/gateway/api/config/WebClientConfiguration.kt
@@ -1,17 +1,20 @@
 package com.gateway.api.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class WebClientConfiguration {
+class WebClientConfiguration(
+    @Value("\${gateway.user_service.url}") val url: String,
+) {
 
     private final var port = 8282
     private final var api = "/api/v1/user"
 
     @Bean
     fun webClient() = WebClient.builder()
-        .baseUrl("http://localhost:$port/$api")
+        .baseUrl("http://$url:$port/$api")
         .build()
 }
