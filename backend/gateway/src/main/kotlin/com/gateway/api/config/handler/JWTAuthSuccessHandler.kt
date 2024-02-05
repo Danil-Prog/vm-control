b04894c0208a3da8747eba77f02b8b6aa422f78a
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-02-05 21:25:08 +0700

    Настроен cookie для refresh токена

--- backend/gateway/src/main/kotlin/com/gateway/api/config/handler/JWTAuthSuccessHandler.kt
+++ backend/gateway/src/main/kotlin/com/gateway/api/config/handler/JWTAuthSuccessHandler.kt
@@ -1,10 +1,14 @@
 package com.gateway.api.config.handler
 
+import com.gateway.api.handlers.AuthHandler
 import com.gateway.api.response.AuthenticationResponse
 import com.gateway.api.service.JwtService
+import com.gateway.api.utils.HttpConstants
+import com.gateway.api.utils.HttpConstants.Companion.REFRESH_TOKEN
 import kotlinx.coroutines.reactor.mono
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.server.WebFilterExchange
@@ -12,6 +16,7 @@ import org.springframework.security.web.server.authentication.ServerAuthenticati
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
+import java.time.Duration
 
 @Component
 class JWTAuthSuccessHandler(private val jwtService: JwtService) : ServerAuthenticationSuccessHandler {
@@ -37,12 +42,20 @@ class JWTAuthSuccessHandler(private val jwtService: JwtService) : ServerAuthenti
                 val authenticationResponse = AuthenticationResponse(accessToken, refreshToken).toByteArray()
                 val data = exchange.response.bufferFactory().wrap(authenticationResponse)
 
+                val responseCookie = ResponseCookie.from(REFRESH_TOKEN, refreshToken)
+                    .httpOnly(true)
+                    .path("/")
+                    .maxAge(Duration.ofMillis(jwtService.expirationRefreshToken))
+                    .domain("localhost")
+                    .build()
+
                 with(exchange.response) {
                     headers.setBearerAuth(accessToken)
-                    headers.set("Refresh-Token", refreshToken)
+                    headers.set(REFRESH_TOKEN, refreshToken)
                     headers.contentType = MediaType.APPLICATION_JSON
                 }
 
+                exchange.response.cookies.set(REFRESH_TOKEN, responseCookie)
                 exchange.response.writeWith(Mono.just(data)).block()
             }
 
--- backend/gateway/src/main/kotlin/com/gateway/api/handlers/AuthHandler.kt
+++ backend/gateway/src/main/kotlin/com/gateway/api/handlers/AuthHandler.kt
@@ -2,19 +2,24 @@ package com.gateway.api.handlers
 
 import com.gateway.api.response.AuthenticationResponse
 import com.gateway.api.service.JwtService
+import com.gateway.api.utils.HttpConstants
+import com.gateway.api.utils.HttpConstants.Companion.REFRESH_TOKEN
 import org.slf4j.Logger
+import org.springframework.boot.web.server.Cookie
+import org.springframework.http.ResponseCookie
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import java.time.Duration
 
 @Component
 class AuthHandler(private val logger: Logger, private val jwtService: JwtService) {
 
     suspend fun refreshAuthToken(request: ServerRequest): ServerResponse {
-        val refresh = request.headers().firstHeader(REFRESH_TOKEN) ?: throw RuntimeException("Refresh token is missing request header ")
+        val refresh = request.cookies().getFirst(REFRESH_TOKEN)?.value ?: throw RuntimeException("Refresh token is missing request header ")
         val isValid = jwtService.valid(refresh)
-        
+
         if (isValid) {
             val username = jwtService.getUsernameRefreshToken(refresh)
             val roles = jwtService.getRolesRefreshToken(refresh).map { it.authority }.toTypedArray()
@@ -24,17 +29,22 @@ class AuthHandler(private val logger: Logger, private val jwtService: JwtService
 
             val authenticationResponse = AuthenticationResponse(accessToken, refreshToken)
 
+            val responseCookie = ResponseCookie.from(REFRESH_TOKEN, refreshToken)
+                .httpOnly(true)
+                .path("/")
+                .maxAge(Duration.ofMillis(jwtService.expirationRefreshToken))
+                .domain("localhost")
+                .build()
+
             return ServerResponse.ok().headers {
                 it.setBearerAuth(accessToken)
                 it.set(REFRESH_TOKEN, refreshToken)
+            }.cookies {
+                it.set(REFRESH_TOKEN, responseCookie)
             }.bodyValueAndAwait(authenticationResponse)
         }
 
         logger.error("Refresh token is not valid")
         return ServerResponse.badRequest().bodyValueAndAwait("Error update refresh token")
-    }
-
-    companion object {
-        private const val REFRESH_TOKEN = "Refresh-Token"
     }
 }
