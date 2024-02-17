2f574979ba6b8d4bdc39db308c6cda2e1452914d
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-02-18 03:05:32 +0700

    update: - clean code

--- backend/user-service/src/main/kotlin/com/user/api/service/UserService.kt
+++ backend/user-service/src/main/kotlin/com/user/api/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.user.api.service
 
+import com.user.api.exception.UserNotFoundException
 import com.user.api.model.User
 import com.user.api.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service
 class UserService(private val userRepository: UserRepository) {
 
     fun getUserByUsername(username: String): User {
-        return userRepository.getUserByUsername(username).orElseThrow { RuntimeException("Sorry, user not found: $username") }
+        return userRepository.getUserByUsername(username)
+            .orElseThrow { UserNotFoundException("Sorry, user not found: $username") }
     }
 }
