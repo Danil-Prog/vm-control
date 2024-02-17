59f01fdf204720374d849a7cbc43228b6897f5ce
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-02-18 03:04:35 +0700

    update: - настройка клиента для сервисов

--- backend/gateway/src/main/kotlin/com/gateway/api/config/WebClientConfiguration.kt
+++ backend/gateway/src/main/kotlin/com/gateway/api/config/WebClientConfiguration.kt
@@ -1,20 +1,29 @@
 package com.gateway.api.config
 
-import org.springframework.beans.factory.annotation.Value
+import com.gateway.api.exception.ClientException
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Configuration
-class WebClientConfiguration(
-    @Value("\${gateway.user_service.url}") val url: String,
-) {
-
-    private final var port = 8282
-    private final var api = "/api/v1/user"
-
+class WebClientConfiguration {
     @Bean
-    fun webClient() = WebClient.builder()
-        .baseUrl("http://user-service:$port/$api")
-        .build()
+    fun webClientBuilder() = WebClient.builder()
+        .filter(errorHandler())
+
+    companion object {
+        fun errorHandler(): ExchangeFilterFunction {
+            return ExchangeFilterFunction.ofResponseProcessor { clientResponse ->
+                if (clientResponse.statusCode().is4xxClientError) {
+                    clientResponse.bodyToMono(String::class.java).flatMap { error ->
+                        Mono.error(ClientException(error))
+                    }
+                } else {
+                    Mono.just(clientResponse)
+                }
+            }
+        }
+    }
 }
